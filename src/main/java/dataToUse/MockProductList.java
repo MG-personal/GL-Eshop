344c6ebfc75c0b8f6f69a75b21773a7d7ad6982c
Han Kieu <ngochan.kieu@gmail.com>
2026-05-05 05:31:23 +0200

    commit: add implementation of dataToUse

--- src/main/java/dataToUse/MockProductList.java
+++ src/main/java/dataToUse/MockProductList.java
@@ -1,11 +1,14 @@
 package dataToUse;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class MockProductList implements ProductListInt{
+public class MockProductList {
 
-
-
-
-
-
+    public static List<Product> getProducts() {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Computer", 790));
+        products.add(new Product("Keyboard", 120));
+        return products;
+    }
 }
--- src/main/java/dataToUse/ProductListInt.java
+++
@@ -1,4 +0,0 @@
-package dataToUse;
-
-public interface ProductListInt {
-}
