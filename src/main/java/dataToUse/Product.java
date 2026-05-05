631eb19c2403c4b7f7bc72215fa893f40fc9d5f6
Han Kieu <ngochan.kieu@gmail.com>
2026-05-05 03:25:59 +0200

    commit: add constructor and getters

--- src/main/java/dataToUse/Product.java
+++ src/main/java/dataToUse/Product.java
@@ -1,6 +1,19 @@
 package dataToUse;
 
 public class Product {
-    String productName;
-    int productPrice;
+    private String name;
+    private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
